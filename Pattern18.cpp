b6fcaca4328f3ff20fcc4d8f87a21da42b7a319a
Barnik Chakraborty <barnikc79@gmail.com>
2025-02-23 18:26:27 +0530

    Fixed code for Pattern 18

--- Pattern18.cpp
+++ Pattern18.cpp
@@ -9,7 +9,7 @@ int main()
 
     for(int i=0;i<n;i++)
     {
-        for(char ch = 'E'-i;ch<='E';ch++)
+        for(char ch='A'+n-1;ch>='A'+n-i-1;ch--)
         {
             cout << ch << " ";
         }
